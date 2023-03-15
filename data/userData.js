1b3c3b8b658a731bcd85c9a78e040f2175e4b8d2
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-15 13:44:02 +0700

    Project Setup - Add data directory to contain User Data

---
+++ data/userData.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "CREATE_USER_DATA":
+    {
+        "id": 123,
+        "username": "Fatima123",
+        "firstName": "Fatima",
+        "lastName": "Zza",
+        "email": "fatima@mail.com",
+        "password": "Pass123",
+        "phone": "081234567890",
+        "userStatus": 0
+    }
+}
