509440adf6ed25d92332fe988ef5761a805f9709
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-15 13:47:14 +0700

    All API Testing POST - Add Test Case & Assertion for Post Create User

--- test/api/petStoreAPITesting.js
+++ test/api/petStoreAPITesting.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const request = require("supertest");
 const baseUrl = require("../../env");
+const data = require('../../data/userData.js')
 
 
 describe('PetStore Get Inventory - GET', () => {
@@ -17,6 +18,24 @@ describe('PetStore Get Inventory - GET', () => {
         expect((await response).body).to.haveOwnProperty('available')
         expect((await response).body).to.haveOwnProperty('unavailable')
         expect((await response).body).to.haveOwnProperty('pending')
+    })
+})
+
+describe('PetStore Create User - POST', () => {
+    const response = request(baseUrl())
+        .post('/user')
+        .send(data.CREATE_USER_DATA);
+
+    console.log(data.CREATE_USER_DATA);
+
+    it('response status equal to 200', async () => {
+        console.log((await response).status);
+        expect((await response).status).to.equal(200)
+    })
+
+    it('response body has property message', async () => {
+        console.log((await response).body);
+        expect((await response).body).to.haveOwnProperty('message')
     })
 })
 
