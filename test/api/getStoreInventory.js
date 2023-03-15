cae85e87ecf8b861f8e4c9ddf5fff157f32594c9
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-15 12:48:53 +0700

    GET Store Inventory - Add Alternative Assertion using Async Await

--- test/api/getStoreInventory.js
+++ test/api/getStoreInventory.js
@@ -17,4 +17,16 @@ describe("PetStore Get Inventory - GET", () => {
         assert.equal((await response).status, 200);
         expect((await response).statusCode).equal(200);
     })
+
+    // test case alternative
+    it("1B Status Code should be 200", async () => {
+        // request to URL
+        const response = await request(baseUrl)
+            .get('/store/inventory');
+        console.log(response.status);
+        console.log(response.body);
+        // assertion
+        assert.equal(response.status, 200);
+        expect(response.statusCode).equal(200);
+    })
 })
